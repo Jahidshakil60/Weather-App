21a603eae24b2e2452d723e5956e7b520b9902db
Jahid Hasan <theshakil53@gmail.com>
2022-03-02 11:59:33 +0600

    added some function and fetch api

---
+++ app.js
@@ -0,0 +1,63 @@
+window.addEventListener('load' , () => {
+
+  let bodyy = document.querySelector('body') 
+  let temparature = document.querySelector('.temp-degee');
+  let location = document.querySelector('.location-timezone');
+  let description = document.querySelector('.temp-description');
+  let searchValue = document.querySelector('.search-value');
+  let searchButton = document.querySelector('.search-button');
+
+  let city ;
+
+  searchButton.addEventListener('click' , function () {
+    city = searchValue.value; 
+  //  })
+
+  const api = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d968d0bf8e4b382411e3846f6e87d970` ;
+
+  fetch(api).then(
+    response => {
+      return response.json();
+    }
+  ).then(data => {
+
+    //console.log(data);
+
+    const {temp} = data.main;
+
+    const {name} = data;
+
+
+    temparature.textContent = temp - 273.15;
+
+    location.textContent = name;
+
+  if (temparature.textContent <32 && temparature.textContent >25 ) {
+
+    description.textContent = "It's a nice weather";
+    bodyy.style.background =  linearGradient(rgb(47,158,1), rgb(48,62,190)) ;
+    
+
+  }
+  else if (temparature.textContent >32 ) {
+
+    description.textContent = "it's hot";
+
+  }
+  else if (temparature.textcontent <25) {
+     description.textContent = "It's cold" ;
+   
+  }
+
+  //  searchButton.addEventListener('click' , function () {
+  //   city == searchValue.textContent; 
+  //  })
+
+
+
+    //console.log(temp);
+
+  })
+
+})
+})
